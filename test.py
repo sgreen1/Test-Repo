072ad25bad8a2a2305a5fa5b39c5a04839edae5f
Steven Green <sgreen1@gmail.com>
2017-03-30 10:50:06 -0700

    adding test.py
    	new file:   test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+def reformat_mac(mac):
+    mac0 = mac.split()[0][0]
+    mac1 = mac.split()[0][1]
+    mac2 = mac.split()[0][2]
+    mac3 = mac.split()[0][3]
+    mac4 = mac.split()[0][5]
+    mac5 = mac.split()[0][6]
+    newmac = mac0 + mac1 + ":" + mac2 + mac3 + ":" + mac4 + mac5
+    return newmac.upper()
